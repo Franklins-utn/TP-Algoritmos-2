7a61cbf69e5acc16a1caeaa126f0f14a4a14b82f
Franco Fourmantin <francofourmantin@gmail.com>
2021-10-08 18:08:25 -0300

    El id de los conductores se genera automaticamente

--- main.cpp
+++ main.cpp
@@ -59,7 +59,7 @@ void cargarConductoresEnMemoria(NodoConductor *&listaConductores); //Esta funci�
 void insertarConductorAlFinal(NodoConductor *&listaConductores, Conductor conductor);
 void listarTodosLosConductores(NodoConductor *listaConductores);
 void desactivarConductor(NodoConductor *&listaConductores ,  int conductorId);
-Conductor crearNuevoConductor();
+Conductor crearNuevoConductor(NodoConductor *&listaConductores);
 NodoConductor *obtenerUltimoConductor(NodoConductor *listaConductores);
 
 
@@ -96,7 +96,7 @@ int main()
         switch (opcionMenu)
         {
         case 1:
-            insertarConductorAlFinal(listaConductores , crearNuevoConductor());
+            insertarConductorAlFinal(listaConductores , crearNuevoConductor(listaConductores));
             break;
         case 2:
             listarTodosLosConductores(listaConductores);
@@ -135,6 +135,28 @@ int main()
 
     cout << "\nEl programa ha finalizado." << endl;
     return 0;
+}
+
+void mostrarMenu(int &opcionMenu)
+{
+    opcionMenu = -1;
+
+    cout << "**************************************************" << endl;
+    cout << "Bienvenido al sistema de infracciones de Gobierno Nacional" << endl;
+    cout << "Por favor seleccione una opción: " << endl;
+    cout << "1 - Cargar un nuevo conductor" << endl;
+    cout << "2 - Listar todos los conductores" << endl;
+    cout << "3 - Desactivar un conductor" << endl;
+    cout << "4 - Listar detalle de conductores por provincia" << endl;
+    cout << "5 - Procesar lote de infracciones" << endl;
+    cout << "6 - Mostrar informe de un conductor" << endl;
+    cout << "7 - Exportar informe HTML por conductor entre fechas de registro vencido" << endl;
+    cout << "8 - Exportar informe CSV  por conductor entre fechas de registro vencido" << endl;
+    cout << "9 - Finalizar la jornada" << endl;
+    cout << "0 - Salir del programa" << endl;
+    cout << "**************************************************" << endl;
+
+    cin >> opcionMenu;
 }
 
 
@@ -290,12 +312,10 @@ void listarTodosLosConductores(NodoConductor *listaConductores)
     }
 }
 
-Conductor crearNuevoConductor()
+Conductor crearNuevoConductor(NodoConductor *& listaConductores)
 {
     Conductor conductor;
-
-    cout << "Ingrese el ID del del conductor" << endl;
-    cin >> conductor.conductorId;
+    NodoConductor *ultimo_conductor = obtenerUltimoConductor(listaConductores);
 
     cout << "Ingrese fecha de vencimiento en formato AAAAMMDD " << endl;
     cin >> conductor.fechaVencimiento;
@@ -303,9 +323,18 @@ Conductor crearNuevoConductor()
     cout << "Ingrese por favor un email del conductor" << endl;
     cin >> conductor.email;
 
+
     //Por default estas opciones despues lo podemos discutir si deben ser ingresadas por el usuario
     conductor.totalInfracciones = 0;
     conductor.activo = true;
+    if(ultimo_conductor)
+    {
+        conductor.conductorId = (ultimo_conductor->conductor.conductorId + 1);
+    }
+    else
+    {
+        conductor.conductorId = 1;
+    }
 
     agregarConductorAlArchivo(conductor);
 
@@ -355,27 +384,7 @@ void limpiarConsola()
     system("clear"); //Si estan usando windows creo que aca hay que poner "cls"
 }
 
-void mostrarMenu(int &opcionMenu)
-{
-    opcionMenu = -1;
 
-    cout << "**************************************************" << endl;
-    cout << "Bienvenido al sistema de infracciones de Gobierno Nacional" << endl;
-    cout << "Por favor seleccione una opción: " << endl;
-    cout << "1 - Cargar un nuevo conductor" << endl;
-    cout << "2 - Listar todos los conductores" << endl;
-    cout << "3 - Desactivar un conductor" << endl;
-    cout << "4 - Listar detalle de conductores por provincia" << endl;
-    cout << "5 - Procesar lote de infracciones" << endl;
-    cout << "6 - Mostrar informe de un conductor" << endl;
-    cout << "7 - Exportar informe HTML por conductor entre fechas de registro vencido" << endl;
-    cout << "8 - Exportar informe CSV  por conductor entre fechas de registro vencido" << endl;
-    cout << "9 - Finalizar la jornada" << endl;
-    cout << "0 - Salir del programa" << endl;
-    cout << "**************************************************" << endl;
-
-    cin >> opcionMenu;
-}
 
 void cargar_infraccion_1(Conductor conductor)
 {
